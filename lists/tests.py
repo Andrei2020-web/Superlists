332543d8d793f6dc935c9963786868a3b8772553
Asus <Asus@Asus>
2022-03-28 14:11:21 +0300

    Первый модульный тест и преобразование url, фиктивное представление.

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,12 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
+class HomePageTest(TestCase):
+    '''тест домашней страницы'''
 
-# Create your tests here.
-class SmokeTest(TestCase):
-    '''тест на токсичность'''
-
-    def test_bad_maths(self):
-        '''тест: неправильные математические расчеты'''
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        '''тест: корневой url преобразуется в представление
+            домашней страницы'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
