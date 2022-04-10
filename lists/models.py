bd0e701033d2ccda3762220b0f306e1f1c5e1f1b
Asus <Asus@Asus>
2022-04-10 19:02:33 +0300

    Переадресация после POST и показ всех элементов в шаблоне.

--- lists/models.py
+++ lists/models.py
@@ -6,4 +6,3 @@ from django.db import models
 class Item(models.Model):
     ''''элемент списка'''
     text = models.TextField(default='')
-    pass
--- lists/tests.py
+++ lists/tests.py
@@ -12,15 +12,38 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         '''тест: можно сохранить post-запрос'''
-        responcse = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', responcse.content.decode())
-        self.assertTemplateUsed(responcse, 'home.html')
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        '''тест: переадресует после post-запроса'''
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        '''тест: сохраняет элементы, только когда нужно'''
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list(self):
+        '''тест: отображаются все эленты списка'''
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
     '''тест модели элемента списка'''
 
     def test_saving_and_retrieving_items(self):
-        '''тест сохранения и получения элементов списка'''
+        '''тест: сохранения и получения элементов списка'''
         first_item = Item()
         first_item.text = 'The first list item'
         first_item.save()
@@ -36,3 +59,14 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+    def test_can_save_a_POST_request(self):
+        '''тест: может сохранить post-запрос'''
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
