28637e13237bb1854f51b3549d62a22e55b22714
Asus <Asus@Asus>
2022-04-12 01:13:36 +0300

    Функциональный тест перенесён в пакет functional_tests, вместо TestCase используется LiveServerTestCase.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
+from django.test import LiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     '''тест нового посетителя'''
 
     def setUp(self):
@@ -27,7 +27,7 @@ class NewVisitorTest(unittest.TestCase):
         # Эдит слышала про крутое новое онлайн-приложение со
         # списком неотложных дел. Она решает оценить его
         # домашнюю страницу
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Она видит, что заголовок и шапка страницы говорят о
         # списках неотложных дел
