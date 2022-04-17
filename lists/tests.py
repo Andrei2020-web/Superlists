3cb26dd66ab252cc1b74d0a5e3aeb54c8b64e0f7
Asus <Asus@Asus>
2022-04-17 14:31:16 +0300

    Упростили home.html, переместили один тест в новый класс в list/tests.py, добавили новое предсавление в view.py, упростили home_page, добавили новый url в urls.py.

--- lists/tests.py
+++ lists/tests.py
@@ -21,22 +21,13 @@ class HomePageTest(TestCase):
         '''тест: переадресует после post-запроса'''
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'],
+                         '/lists/some_list/')
 
     def test_only_saves_items_when_necessary(self):
         '''тест: сохраняет элементы, только когда нужно'''
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_displays_all_list(self):
-        '''тест: отображаются все эленты списка'''
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -69,4 +60,23 @@ class ItemModelTest(TestCase):
         self.assertEqual(new_item.text, 'A new list item')
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/some_list/')
+
+
+class ListViewTest(TestCase):
+    '''тест представления списка'''
+
+    def test_displays_all_items(self):
+        '''тест: отображаются все эленты списка'''
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/some_list/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+    def test_user_list_template(self):
+        '''тест: используется шаблон списка'''
+        response = self.client.get('/lists/some_list/')
+        self.assertTemplateUsed(response, 'list.html')
