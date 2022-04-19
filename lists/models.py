c4d54039ba1d47d3c06684d8be2cae753348f753
Asus <Asus@Asus>
2022-04-19 17:16:47 +0300

    Добавлена модель списка в models.py, в модель элемента добавлен внешний ключ на список, изменен тест модели списка в модульных тестах с учетом новой модели элементов списка, изменено представление для нового списка в views.py.

--- lists/models.py
+++ lists/models.py
@@ -3,6 +3,12 @@ from collections import defaultdict
 from django.db import models
 
 # Create your models here.
+class List(models.Model):
+    ''''Список'''
+    pass
+
+
 class Item(models.Model):
     ''''элемент списка'''
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
