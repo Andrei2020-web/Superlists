f27dbf9a6912dcf158803fb13b3fe3f8b5dc1550
Asus <Asus@Asus>
2022-04-20 19:48:30 +0300

    Новый URL + представление для добавления в существующие списки.

--- superlists/urls.py
+++ superlists/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     # добавлена страница нового списка
     re_path(r'^lists/new$', views.new_list, name='new_list'),
     # добавлена страница созданного списка
-    re_path(r'^lists/some_list/$', views.view_list)
+    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    # добавлена страница добавления нового элемента списка
+    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
