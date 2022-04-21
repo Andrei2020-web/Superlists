5603d97093b9f6ad421c668b34a407e4d61a9a34
Asus <Asus@Asus>
2022-04-21 13:49:23 +0300

    Добавлен файл urls.py для приложения lists + рефакторинг файла urls.py сайта (перенос url, которые применяются в приложении lists в отдельный файл.

--- lists/urls.py
+++ lists/urls.py
@@ -13,15 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path, re_path
-from lists import views as list_views
-from lists import urls as list_urls
+from django.urls import path, re_path
+from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # добавлена домашняя страница
-    re_path(r'^$', list_views.home_page, name='home'),
     # добавлена страница нового списка
-    re_path(r'^lists/', include(list_urls)),
+    re_path(r'^new$', views.new_list, name='new_list'),
+    # добавлена страница созданного списка
+    re_path(r'^(\d+)/$', views.view_list, name='view_list'),
+    # добавлена страница добавления нового элемента списка
+    re_path(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 ]
